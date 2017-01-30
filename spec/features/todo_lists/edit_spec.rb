de729e953ffa1ba138d507ed194c160f54f0eea3
Obelich <obelich@gmail.com>
2017-01-29 20:08:06 -0800

    Se modifico readme

---
+++ spec/features/todo_lists/edit_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'Editing todo list' do
+
+  it 'Update a todo list successfully wit correct information' do
+    visit "todo_lists" # Entrar a una URL
+  end
+
+
+end
