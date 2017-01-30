2b8a140a657af7f780765c7449a8b39e27487a58
Obelich <obelich@gmail.com>
2017-01-29 18:58:42 -0800

    Se simplifico el test para reducir la duplicacion de codigo

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -2,27 +2,30 @@ require 'spec_helper'
 
 describe "Creating todo lists" do
 
+  # Mejoramos el script para que no se este repitiendo
+  def create_todo_list(options={})
+    options[:title] ||= "My todo List"
+    options[:Description] ||= "This is what I`m doing today"
 
-
-  it "redirect to the todo list index page on success" do
     visit "todo_lists" # Entrar a una URL
     click_link "New Todo List" # Hacer click en un enlace con contenga el nombre
     expect(page).to have_content('New Todo List')  # Se espera que en el documento se encuentre el siguiente texto
 
-    fill_in "Title", with: "My todo List" # Se llena un input con el name title
-    fill_in "Description", with: "This is what I`m doing today" # Se llena input con el name Description
+    fill_in "Title", with: options[:title] # Se llena un input con el name title
+    fill_in "Description", with: options[:Description] # Se llena input con el name Description
     click_button "Create Todo list" # Se da click en el boton de submit
+  end
+
+
+  it "redirect to the todo list index page on success" do
+    create_todo_list
 
     expect(page).to have_content('My todo List')
   end
 
   it "Display an error when the todo list has no title" do
     expect(TodoList.count).to eq(0)
-    visit "todo_lists" # Entrar a una URL
-    click_link "New Todo List" # Hacer click en un enlace con contenga el nombre
-    fill_in "Title", with: ""
-    fill_in "Description", with: "This is what I`m doing today" # Se llena input con el name Description
-    click_button "Create Todo list" # Se da click en el boton de submit
+    create_todo_list title: ""
 
     expect(page).to have_content('error')
     expect(TodoList.count).to eq(0)
@@ -33,12 +36,10 @@ describe "Creating todo lists" do
   end
 
   it "Display an error when the todo list has a title less than 3 characters" do
+
     expect(TodoList.count).to eq(0)
-    visit "todo_lists" # Entrar a una URL
-    click_link "New Todo List" # Hacer click en un enlace con contenga el nombre
-    fill_in "Title", with: "Hi"
-    fill_in "Description", with: "This is what I`m doing today" # Se llena input con el name Description
-    click_button "Create Todo list" # Se da click en el boton de submit
+    create_todo_list title: "Hi"
+
 
     expect(page).to have_content('error')
     expect(TodoList.count).to eq(0)
@@ -52,11 +53,8 @@ describe "Creating todo lists" do
 
   it "Display an error when the todo list has no description" do
     expect(TodoList.count).to eq(0)
-    visit "todo_lists" # Entrar a una URL
-    click_link "New Todo List" # Hacer click en un enlace con contenga el nombre
-    fill_in "Title", with: "Grosery List"
-    fill_in "Description", with: "" # Se deja el contenido en blanco
-    click_button "Create Todo list" # Se da click en el boton de submit
+
+    create_todo_list title: "Grosery List", Description: ""
 
     expect(page).to have_content('error')
     expect(TodoList.count).to eq(0)
@@ -68,11 +66,8 @@ describe "Creating todo lists" do
 
   it "Display an error when the todo list has description less than 5 characters" do
     expect(TodoList.count).to eq(0)
-    visit "todo_lists" # Entrar a una URL
-    click_link "New Todo List" # Hacer click en un enlace con contenga el nombre
-    fill_in "Title", with: "Grosery List"
-    fill_in "Description", with: "food" # Se deja el contenido en blanco
-    click_button "Create Todo list" # Se da click en el boton de submit
+
+    create_todo_list  title: "Grosery List", Description: "food"
 
     expect(page).to have_content('error')
     expect(TodoList.count).to eq(0)
