a772ef552a0d879adc58e2be27e622c34b6137b0
Obelich <obelich@gmail.com>
2017-01-29 18:44:36 -0800

    Se agregaron mas test con sus respectiavs validaciones

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,3 +1,4 @@
 class TodoList < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, length: {minimum: 3}
+  validates :description, presence: true, length: {minimum: 5}
 end
--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -28,7 +28,57 @@ describe "Creating todo lists" do
     expect(TodoList.count).to eq(0)
 
     visit "todo_lists"
+    expect(page).to_not have_content('This is what I`m doing today') # Aquí se espera que no contenga este texto en la pagina
+
+  end
+
+  it "Display an error when the todo list has a title less than 3 characters" do
+    expect(TodoList.count).to eq(0)
+    visit "todo_lists" # Entrar a una URL
+    click_link "New Todo List" # Hacer click en un enlace con contenga el nombre
+    fill_in "Title", with: "Hi"
+    fill_in "Description", with: "This is what I`m doing today" # Se llena input con el name Description
+    click_button "Create Todo list" # Se da click en el boton de submit
+
+    expect(page).to have_content('error')
+    expect(TodoList.count).to eq(0)
+
+    visit "todo_lists"
     expect(page).to_not have_content('This is what I`m doing today')
+
+  end
+
+
+
+  it "Display an error when the todo list has no description" do
+    expect(TodoList.count).to eq(0)
+    visit "todo_lists" # Entrar a una URL
+    click_link "New Todo List" # Hacer click en un enlace con contenga el nombre
+    fill_in "Title", with: "Grosery List"
+    fill_in "Description", with: "" # Se deja el contenido en blanco
+    click_button "Create Todo list" # Se da click en el boton de submit
+
+    expect(page).to have_content('error')
+    expect(TodoList.count).to eq(0)
+
+    visit "todo_lists"
+    expect(page).to_not have_content('Grosery List')
+
+  end
+
+  it "Display an error when the todo list has description less than 5 characters" do
+    expect(TodoList.count).to eq(0)
+    visit "todo_lists" # Entrar a una URL
+    click_link "New Todo List" # Hacer click en un enlace con contenga el nombre
+    fill_in "Title", with: "Grosery List"
+    fill_in "Description", with: "food" # Se deja el contenido en blanco
+    click_button "Create Todo list" # Se da click en el boton de submit
+
+    expect(page).to have_content('error')
+    expect(TodoList.count).to eq(0)
+
+    visit "todo_lists"
+    expect(page).to_not have_content('Grosery List')
 
   end
 
